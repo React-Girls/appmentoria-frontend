20dd709ad3cb2c4ac4d72a5972ec82c2bcec6d7a
Camila Matos <camila.mila148@gmail.com>
2021-05-19 14:19:15 -0300

    Criado estrutura section 2

--- src/components/Home/SectionWhatIs/index.js
+++ src/components/Home/SectionWhatIs/index.js
@@ -1,8 +1,30 @@
+import WhatIsContainer from "./styled";
+
 export default function SectionWhatIs(){
 
     return(
-        <section>
+        <WhatIsContainer>
             <h1>O que é o mentorialab?</h1>
-        </section>
+            <div>
+                <img src="/assets/home/Ilustração2.svg" alt=""/>
+
+                <div>
+                    <p>
+                        É a maior plataforma de mentorias do Brasil que conecta 
+                        profissionais experientes com profissionais em ascenção 
+                        para um maior desempenho profissional e realização em projetos
+                        e carreiras.
+                    </p>
+                    <p>
+                        Oferecemos agenda, vídeochamada, gravação, fórum, e pagamento
+                        100% seguro. Cuidamos de tudo para você!
+                    </p>
+                    <button>
+                        Quero encontrar um mentor!
+                    </button>
+                </div>
+
+            </div>
+        </WhatIsContainer>
     )
 }
--- src/components/Home/SectionWhatIs/styled.js
+++ src/components/Home/SectionWhatIs/styled.js
@@ -1,7 +1,8 @@
 import styled from 'styled-components'
 
 const WhatIsContainer = styled.div`
-
+    height: 100%;
+    background: #E5E5E5;
 
 `
 
