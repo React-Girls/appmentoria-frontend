90052f651cde0c711b62e741a6a9e5c076f543f1
Camila Matos <camila.mila148@gmail.com>
2021-05-31 16:09:56 -0300

    Corrigido styled sections

--- src/containers/Home/SectionWhatIs/index.js
+++ src/containers/Home/SectionWhatIs/index.js
@@ -1,30 +1,35 @@
-import WhatIsWrapper from "./styled";
+import Title from "../../../components/foundation/Title";
+import Typography from "../../../components/foundation/Typography";
+import { WhatIsWrapper, Container, Div } from "./styled";
 
 export default function SectionWhatIs(){
 
     return(
         <WhatIsWrapper>
-            <h1>O que é o mentorialab?</h1>
-            <div className='Container'> 
+            <Title>O que é o mentorialab?</Title>
+
+            <Container> 
                 <img src="/assets/home/Ilustração2.svg" alt=""/>
 
-                <div>
-                    <p>
+                <Div>
+                    <Typography>
                         É a maior plataforma de mentorias do Brasil que conecta 
                         profissionais experientes com profissionais em ascenção 
                         para um maior desempenho profissional e realização em projetos
                         e carreiras.
-                    </p>
-                    <p>
+                    </Typography>
+                    
+                    <Typography>
                         Oferecemos agenda, vídeochamada, gravação, fórum, e pagamento
                         100% seguro. Cuidamos de tudo para você!
-                    </p>
+                    </Typography>
+
                     <button>
                         Quero encontrar um mentor!
                     </button>
-                </div>
+                </Div>
 
-            </div>
+            </Container>
         </WhatIsWrapper>
     )
 }
--- src/containers/Home/SectionWhatIs/styled.js
+++ src/containers/Home/SectionWhatIs/styled.js
@@ -1,6 +1,6 @@
 import styled from 'styled-components'
 
-const WhatIsWrapper = styled.div`
+export const WhatIsWrapper = styled.div`
     height: 100%;
     background: #E5E5E5;
 
@@ -9,18 +9,9 @@ const WhatIsWrapper = styled.div`
     align-items: baseline;
     justify-content: center;
 
-    h1{
-        font-family: Roboto;
-        font-style: normal;
-        font-weight: normal;
-        font-size: 60px;
-        line-height: 50px; /* or 83% */
+`
 
-        color: #0062B9;
-
-    }
-
-    .Container{
+export const Container = styled.div`
         width: 100%;
         display: flex;
         align-items: center;
@@ -30,27 +21,15 @@ const WhatIsWrapper = styled.div`
             width: 595px;
             height: 597px;
         }
-
-        div{
-            width: 587px;
-            height: 100%;
-
-            display: flex;
-            flex-direction: column;
-            justify-content: center;
-
-            p{
-                font-style: normal;
-                font-weight: normal;
-                font-size: 28px;
-                line-height: 50px;/* or 179% */
-
-                color: #000000;
-            }
-        }
-    }
-
-
+    
 `
 
-export default WhatIsWrapper
+export const Div = styled.div`
+        width: 587px;
+        height: 100%;
+
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+`
+
